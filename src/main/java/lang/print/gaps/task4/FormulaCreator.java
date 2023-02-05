307307471727f89d1e9cc65f315ff7bfca5981c2
Bago0 <jgamadzebagrati@gmail.com>
2023-02-05 22:25:12 +0400

    exercise 5

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte nine = 9;
+        char character1 = 'a';
+        byte two = 2;
+        char character2 = (char) Math.pow('a',2);
+        byte five = 5;
+        char character3 = 'b';
+        byte seven = 7;
+        byte four = 4;
+
+        System.out.println("("+nine+character2+"-"+five+character3+"+"+two+"+"+character1+"-"+seven+")"+"*"+"("+"("+character1+"+"+character3+"-"+four+character1+character3+")"+"/"+two+")");
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char first  = 99;
+        char second = 104;
+        char third = 97;
+        char fourth = 114;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(fourth);
     }
 }
